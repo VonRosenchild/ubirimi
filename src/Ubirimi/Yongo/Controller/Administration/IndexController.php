7267938d261067572e51d172a01dcabbe171fa49
domnulnopcea <domnulnopcea@gmail.com>
2014-07-08 20:13:38 +0300

    fix yongo administration project viewing

--- src/Ubirimi/Yongo/Controller/Administration/IndexController.php
+++ src/Ubirimi/Yongo/Controller/Administration/IndexController.php
@@ -2,6 +2,8 @@
     use Ubirimi\SystemProduct;
     use Ubirimi\Util;
     use Ubirimi\Yongo\Repository\Project\Project;
+    use Ubirimi\Repository\Client;
+    use Ubirimi\Yongo\Repository\Permission\Permission;
 
     Util::checkUserIsLoggedInAndRedirect();
 
@@ -10,8 +12,16 @@
     $hasYongoAdministerProjectsPermission = $session->get('user/yongo/is_global_project_administrator');
 
     $menuSelectedCategory = 'administration';
-    $last5Projects = Project::getLast5ByClientId($clientId);
-    $countProjects = Project::getCount($clientId);
+
+    if ($hasYongoGlobalAdministrationPermission && $hasYongoGlobalSystemAdministrationPermission) {
+        $projects = Client::getProjects($clientId, 'array');
+        $last5Projects = Project::getLast5ByClientId($clientId);
+        $countProjects = Project::getCount($clientId);
+    } else if ($hasYongoAdministerProjectsPermission) {
+        $projects = Client::getProjectsByPermission($clientId, $loggedInUserId, Permission::PERM_ADMINISTER_PROJECTS, 'array');
+        $countProjects = count($projects);
+        $last5Projects = null;
+    }
 
     $sectionPageTitle = $session->get('client/settings/title_name') . ' / ' . SystemProduct::SYS_PRODUCT_YONGO_NAME . ' / Administration';
 
