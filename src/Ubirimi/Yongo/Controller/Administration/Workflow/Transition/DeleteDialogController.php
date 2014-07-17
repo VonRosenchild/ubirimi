b90e8e37e63408ab7deb378677ce2c23d1c65ce0
Nopcea Francisc <domnuprofesor@gmail.com>
2014-07-17 13:39:04 +0300

    Transition and Step controllers refactored

--- src/Ubirimi/Yongo/Controller/Administration/Workflow/Transition/DeleteDialogController.php
+++ src/Ubirimi/Yongo/Controller/Administration/Workflow/Transition/DeleteDialogController.php
@@ -1,1 +1,16 @@
-Are you sure you want to delete this transition?
+<?php
+
+namespace Ubirimi\Yongo\Controller\Administration\Workflow\Transition;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Ubirimi\UbirimiController;
+
+class DeleteDialogController extends UbirimiController
+{
+    public function indexAction(Request $request, SessionInterface $session)
+    {
+        return new Response('Are you sure you want to delete this transition?');
+    }
+}
--- src/Ubirimi/Yongo/Controller/Administration/Workflow/Transition/ViewBrowserController.php
+++ src/Ubirimi/Yongo/Controller/Administration/Workflow/Transition/ViewBrowserController.php
@@ -1,20 +1,33 @@
 <?php
-    use Ubirimi\SystemProduct;
-    use Ubirimi\Util;
-    use Ubirimi\Yongo\Repository\Workflow\Workflow;
 
-    Util::checkUserIsLoggedInAndRedirect();
+namespace Ubirimi\Yongo\Controller\Administration\Workflow\Transition;
 
-    $workflowDataId = $_GET['id'];
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Ubirimi\SystemProduct;
+use Ubirimi\UbirimiController;
+use Ubirimi\Util;
+use Ubirimi\Yongo\Repository\Workflow\Workflow;
 
-    $workflowData = Workflow::getDataById($workflowDataId);
-    $workflow = Workflow::getMetaDataById($workflowData['workflow_id']);
+class ViewBrowserController extends UbirimiController
+{
+    public function indexAction(Request $request, SessionInterface $session)
+    {
+        Util::checkUserIsLoggedInAndRedirect();
 
-    if ($workflow['client_id'] != $clientId) {
-        header('Location: /general-settings/bad-link-access-denied');
-        die();
+        $workflowDataId = $request->get('id');
+
+        $workflowData = Workflow::getDataById($workflowDataId);
+        $workflow = Workflow::getMetaDataById($workflowData['workflow_id']);
+
+        if ($workflow['client_id'] != $session->get('client/id')) {
+            return new RedirectResponse('/general-settings/bad-link-access-denied');
+        }
+
+        $menuSelectedCategory = 'issue';
+        $sectionPageTitle = $session->get('client/settings/title_name') . ' / ' . SystemProduct::SYS_PRODUCT_YONGO_NAME . ' / Workflow Transition Browser';
+
+        return $this->render(__DIR__ . '/../../../../Resources/views/administration/workflow/transition/ViewBrowser.php', get_defined_vars());
     }
-
-    $menuSelectedCategory = 'issue';
-    $sectionPageTitle = $session->get('client/settings/title_name') . ' / ' . SystemProduct::SYS_PRODUCT_YONGO_NAME . ' / Workflow Transition Browser';
-    require_once __DIR__ . '/../../../../Resources/views/administration/workflow/transition/ViewTransitionBrowser.php';
+}
