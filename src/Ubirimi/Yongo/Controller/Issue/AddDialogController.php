330d053721e2a6bd55620b681b30644068e1db6d
Nopcea Francisc <domnuprofesor@gmail.com>
2014-07-30 11:12:37 +0300

    AddIssue - moved logic from view to controller

--- src/Ubirimi/Yongo/Controller/Issue/AddDialogController.php
+++ src/Ubirimi/Yongo/Controller/Issue/AddDialogController.php
@@ -3,10 +3,12 @@
 namespace Ubirimi\Yongo\Controller\Issue;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Ubirimi\Repository\Client;
 use Ubirimi\SystemProduct;
 use Ubirimi\UbirimiController;use Ubirimi\Util;
+use Ubirimi\Yongo\Repository\Issue\SystemOperation;
 use Ubirimi\Yongo\Repository\Permission\Permission;
 use Ubirimi\Yongo\Repository\Project\Project;
 
@@ -16,27 +18,31 @@ class AddDialogController extends UbirimiController
     {
         Util::checkUserIsLoggedInAndRedirect();
 
-        $canCreateIssue = $request->get('can_create');
-
         $selectedProjectId = $session->get('selected_project_id');
+        $sysOperationId = SystemOperation::OPERATION_CREATE;
 
-        if ($canCreateIssue) {
-            if ($session->get('selected_product_id') == SystemProduct::SYS_PRODUCT_YONGO) {
-                $projects = Client::getProjectsByPermission($session->get('client/id'), $session->get('user/id'), Permission::PERM_CREATE_ISSUE);
-            } else {
-                $projects = Client::getProjects($session->get('client/id'), null, null, true);
-            }
-
-            $projectData = Project::getById($selectedProjectId);
-            $issueTypes = Project::getIssueTypes($selectedProjectId, 0);
-
-            $firstIssueType = $issueTypes->fetch_array(MYSQLI_ASSOC);
-            $issueTypeId = $firstIssueType['id'];
-            $issueTypes->data_seek(0);
-
-
-            $typeId = null;
+        if (0 == $request->get('can_create')) {
+            return new Response('<div class="infoBox">You do not have any projects with the permission to create an issue.</div>');
         }
+
+        if ($session->get('selected_product_id') == SystemProduct::SYS_PRODUCT_YONGO) {
+            $projects = Client::getProjectsByPermission(
+                $session->get('client/id'),
+                $session->get('user/id'),
+                Permission::PERM_CREATE_ISSUE
+            );
+        } else {
+            $projects = Client::getProjects($session->get('client/id'), null, null, true);
+        }
+
+        $projectData = Project::getById($selectedProjectId);
+        $issueTypes = Project::getIssueTypes($selectedProjectId, 0);
+
+        $firstIssueType = $issueTypes->fetch_array(MYSQLI_ASSOC);
+        $issueTypeId = $firstIssueType['id'];
+        $issueTypes->data_seek(0);
+
+        $typeId = null;
 
         return $this->render(__DIR__ . '/../../Resources/views/issue/AddDialog.php', get_defined_vars());
     }
--- src/Ubirimi/Yongo/Resources/views/issue/AddDialog.php
+++ src/Ubirimi/Yongo/Resources/views/issue/AddDialog.php
@@ -1,45 +1,41 @@
 <?php
     use Ubirimi\Yongo\Repository\Field\Field;
     use Ubirimi\Yongo\Repository\Issue\SystemOperation;
-
-    $sysOperationId = SystemOperation::OPERATION_CREATE;
 ?>
-<?php if ($canCreateIssue): ?>
-    <div style="padding-left: 0px; margin-right: 0px; min-height: 300px;">
-        <div id="errosMandatoryFieldsNotPresentOnScreen"></div>
-        <div id="messageIssueCreatedDialog" class="messageGreen" style="padding: 8px; display: none"></div>
-        <table border="0" cellpadding="2" cellspacing="0" id="tableFieldList" class="modal-table">
-            <tr>
-                <td width="170">Project</td>
-                <td>
-                    <select id="field_type_project" name="project" class="inputTextCombo">
-                        <?php while ($project = $projects->fetch_array(MYSQLI_ASSOC)): ?>
-                            <option <?php if ($selectedProjectId == $project['id']) echo 'selected="selected"' ?> value="<?php echo $project['id'] ?>"><?php echo $project['name'] ?></option>
-                        <?php endwhile ?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>Issue Type</td>
-                <td>
-                    <select id="field_type_<?php echo Field::FIELD_ISSUE_TYPE_CODE ?>" name="type" class="inputTextCombo">
-                        <?php while ($type = $issueTypes->fetch_array(MYSQLI_ASSOC)): ?>
-                            <option <?php if ($typeId && $typeId == $type['id']) echo 'selected="selected"' ?> value="<?php echo $type['id'] ?>"><?php echo $type['name'] ?></option>
-                        <?php endwhile ?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2"><hr size="1" /></td>
-            </tr>
 
-            <?php
-                require_once __DIR__ . '/_dialogCreate.php';
-            ?>
-        </table>
-    </div>
-    <input type="hidden" value="<?php echo $selectedProjectId ?>" id="project_id" />
-    <input type="hidden" value="<?php echo SystemOperation::OPERATION_CREATE ?>" id="operation_id" />
-<?php else: ?>
-    <div class="infoBox">You do not have any projects with the permission to create an issue.</div>
-<?php endif ?>
+<div style="padding-left: 0px; margin-right: 0px; min-height: 300px;">
+    <div id="errosMandatoryFieldsNotPresentOnScreen"></div>
+    <div id="messageIssueCreatedDialog" class="messageGreen" style="padding: 8px; display: none"></div>
+    <table border="0" cellpadding="2" cellspacing="0" id="tableFieldList" class="modal-table">
+        <tr>
+            <td width="170">Project</td>
+            <td>
+                <select id="field_type_project" name="project" class="inputTextCombo">
+                    <?php while ($project = $projects->fetch_array(MYSQLI_ASSOC)): ?>
+                        <option <?php if ($selectedProjectId == $project['id']) echo 'selected="selected"' ?> value="<?php echo $project['id'] ?>"><?php echo $project['name'] ?></option>
+                    <?php endwhile ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Issue Type</td>
+            <td>
+                <select id="field_type_<?php echo Field::FIELD_ISSUE_TYPE_CODE ?>" name="type" class="inputTextCombo">
+                    <?php while ($type = $issueTypes->fetch_array(MYSQLI_ASSOC)): ?>
+                        <option <?php if ($typeId && $typeId == $type['id']) echo 'selected="selected"' ?> value="<?php echo $type['id'] ?>"><?php echo $type['name'] ?></option>
+                    <?php endwhile ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2"><hr size="1" /></td>
+        </tr>
+
+        <?php
+            require_once __DIR__ . '/_dialogCreate.php';
+        ?>
+    </table>
+</div>
+
+<input type="hidden" value="<?php echo $selectedProjectId ?>" id="project_id" />
+<input type="hidden" value="<?php echo SystemOperation::OPERATION_CREATE ?>" id="operation_id" />
