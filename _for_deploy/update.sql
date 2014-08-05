1edbe24fe6f9446f386d40036056c63ed33a7422
Flavius Nopcea <domnulnopcea@gmail.com>
2014-08-05 15:56:04 +0300

    update sql file

--- _for_deploy/update.sql
+++ _for_deploy/update.sql
@@ -1,3 +1,6 @@
+
+====================== rulate pe live===========================
+
 ALTER TABLE `project` CHANGE `help_desk_enabled_flag` `help_desk_enabled_flag` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0';
 
 ALTER TABLE `client` ADD UNIQUE(`company_domain`);
