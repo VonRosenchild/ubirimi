c9f0a5b0ed38164b91031ea85a332ef4c0d05717
Nopcea Francisc <domnuprofesor@gmail.com>
2014-08-08 10:11:38 +0300

    update bugzilla cron much improved

--- cron/update_bugzilla.php
+++ cron/update_bugzilla.php
@@ -1,12 +1,14 @@
 <?php
 
-
 use Ubirimi\Container\UbirimiContainer;
 use Ubirimi\Repository\GeneralTaskQueue;
 use Ubirimi\Yongo\Repository\Issue\Issue;
 use Ubirimi\Yongo\Repository\Issue\IssueComponent;
 use Ubirimi\Yongo\Repository\Issue\IssueVersion;
 use Ubirimi\Repository\User\User;
+use Ubirimi\Yongo\Repository\Issue\IssueCustomField;
+use Ubirimi\Yongo\Repository\Project\Project;
+use Ubirimi\Yongo\Repository\Issue\IssueComment;
 
 /* check locking mechanism */
 if (file_exists('update_bugzilla.lock')) {
@@ -24,46 +26,66 @@ $bugzillaConn = UbirimiContainer::get()['bugzilla.connection'];
 
 $bugzillaConn->autocommit(false);
 
-$pilotTeamStartDate = date('2014-07-15 00:00:00');
+$PILOT_TEAM_START_DATE = date('2014-07-31 00:00:00');
+
+$severityConstants = array(
+    1 => 'S1.Blocker (crashes)',
+    2 => 'S2.Critical (unusable)',
+    3 => 'S3.normal (not fully functional)',
+    4 => 'S4.minor (cosmetic)',
+    5 => 'S5.Enhancement (nice to have)',
+    6 => 'major'
+);
 
 try {
-    $bugzillaConn->commit();
-
-    $newIssues = Issue::getByParameters(array(
-        'date_created_after' => $pilotTeamStartDate
+    $newIssuesUpdated = Issue::getByParameters(array(
+        'date_updated_after' => $PILOT_TEAM_START_DATE
     ));
 
-    if (null !== $newIssues) {
-        $newIssues = $newIssues->fetch_all(MYSQLI_ASSOC);
+    $newIssuesCreated = Issue::getByParameters(array(
+        'date_created_after' => $PILOT_TEAM_START_DATE
+    ));
 
+    $newIssuesUpdated = $newIssuesUpdated->fetch_all(MYSQLI_ASSOC);
+    $newIssuesCreated = $newIssuesCreated->fetch_all(MYSQLI_ASSOC);
+
+    if (0 == count($newIssuesCreated && 0 == $newIssuesUpdated)) {
+        die("\nNo new issues\n");
+    }
+
+    $newIssues = $newIssuesUpdated;
+
+    foreach ($newIssuesCreated as $newIssueCreated) {
+        $found = false;
         foreach ($newIssues as $newIssue) {
-            /* check if the issue is new all together or only some fields have changed */
-
-            $bug = getBugzillaBugBasedOnUbirimiIssue($newIssue['summary'], $newIssue['date_created']);
-            $issueComponent = IssueComponent::getByIssueIdAndProjectId($newIssue['id'], $newIssue['issue_project_id']);
-            $issueVersion = IssueVersion::getByIssueIdAndProjectId($newIssue['id'], $newIssue['issue_project_id'], 'fdsfdsfds');
-            /* bugzilla does not have the issue yet */
-            if (null === $bug) {
-                insertBugzillaBug(
-                    $newIssue['user_assigned_id'],
-                    ';fjsdh;fsdjkh',
-                    $newIssue['status_name'],
-                    $issueComponent[0]['id'],
-                    $newIssue['date_created'],
-                    $newIssue['date_updated'],
-                    $newIssue['priority_name'],
-                    $newIssue['issue_project_id'],
-                    $newIssue['reporter'],
-                    $newIssue['resolution_name'],
-                    $newIssue['summary'],
-                    $issueVersion
-
-                );
-            } else {
-
+            if ($newIssue['id'] == $newIssueCreated['id']) {
+                $found = true;
             }
         }
+
+        if (false === $found) {
+            $newIssues[] = $newIssueCreated;
+        }
     }
+
+
+    foreach ($newIssues as $newIssue) {
+        /* check if the issue is new all together or only some fields have changed */
+        $bug = getBugzillaBugBasedOnUbirimiIssue($newIssue['summary'], $newIssue['date_created']);
+
+        /* bugzilla does not have the issue yet */
+        if (null === $bug) {
+            processNewBugzillaBug($newIssue);
+        } else {
+            /* if is does, delete the bugzilla issue and crete a new one */
+            deleteBugzillaBug($bug['bug_id']);
+
+            processNewBugzillaBug($newIssue);
+        }
+    }
+
+    $bugzillaConn->commit();
+
 } catch (\Exception $e) {
     $bugzillaConn->rollback();
 
@@ -72,6 +94,207 @@ try {
 
 if (null !== $fp) {
     fclose($fp);
+}
+
+function deleteBugzillaBug($bugId)
+{
+    $query = "delete from longdescs where bug_id = ?";
+
+    $stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query);
+
+    $stmt->bind_param("i", $bugId);
+    $stmt->execute();
+
+    $query = "delete from bugs where bug_id = ?";
+
+    $stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query);
+
+    $stmt->bind_param("i", $bugId);
+    $stmt->execute();
+}
+
+function processNewBugzillaBug($newIssue)
+{
+    global $severityConstants;
+
+    $issueComponent = IssueComponent::getByIssueIdAndProjectId($newIssue['id'], $newIssue['issue_project_id']);
+    $issueVersion = IssueVersion::getByIssueIdAndProjectId($newIssue['id'], $newIssue['issue_project_id'], Issue::ISSUE_AFFECTED_VERSION_FLAG);
+    $project = Project::getById($newIssue['issue_project_id']);
+    $severity = IssueCustomField::getCustomFieldsDataByFieldId($newIssue['id'], 29403);
+    $comments = IssueComment::getByIssueId($newIssue['id']);
+
+    if (null !== $issueComponent) {
+        $issueComponent = $issueComponent->fetch_array(MYSQLI_ASSOC);
+    } else {
+        echo "Issue " . $newIssue['summary'] . " skipped. Missing component.\n";
+        return -1;
+    }
+
+    if (null == $newIssue['resolution_name']) {
+        $newIssue['resolution_name'] = '';
+    }
+
+    if (null == $issueVersion) {
+        $issueVersion = 'unspecified';
+    }
+
+    if (isset($severityConstants[$severity['value']])) {
+        $severity = $severityConstants[$severity['value']];
+    } else {
+        $severity = $severityConstants[6];
+    }
+
+    $bugId = insertBugzillaBug(
+        $newIssue['assignee'],
+        $severity,
+        $newIssue['status_name'],
+        $issueComponent,
+        $newIssue['date_created'],
+        $newIssue['date_updated'],
+        $newIssue['priority_name'],
+        $project,
+        $newIssue['reporter'],
+        $newIssue['resolution_name'],
+        $newIssue['summary'],
+        $issueVersion
+    );
+
+    insertBugzillaComments($bugId, $comments, $newIssue['summary']);
+}
+
+function insertBugzillaBug($assigned_to, $bug_severity, $bug_status, $component, $creation_ts, $date_updated, $priority, $project, $reporter, $resolution, $short_desc, $version)
+{
+    $ubirimiUser = User::getById($assigned_to);
+    $ubirimiReporter = User::getById($reporter);
+
+    $bugzillaUser = getBugzillaUserByEmail($ubirimiUser['email']);
+    $bugzillaReporter = getBugzillaUserByEmail($ubirimiReporter['email']);
+
+    $bugzillaProduct = getBuzillaProduct($project);
+    $bugzillaComponent = getBugzillaComponent($component);
+
+    $repPlatform = 'All';
+    $everconfirmed = 1;
+    $bug_file_loc = '';
+    $op_sys = '';
+    if (empty($date_updated)) {
+        $date_updated = '0000-00-00 00:00:00';
+    }
+
+    $query = "INSERT INTO bugs(
+                assigned_to, bug_severity, bug_status, component_id, creation_ts, delta_ts,
+                priority, product_id, reporter, resolution, short_desc, version, rep_platform, everconfirmed, bug_file_loc, op_sys)
+              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    if ($stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query)) {
+        $stmt->bind_param(
+            "ississsiissssiss",
+            $bugzillaUser[0]['userid'], $bug_severity, $bug_status, $bugzillaComponent[0]['id'],
+            $creation_ts, $date_updated, $priority, $bugzillaProduct[0]['id'],
+            $bugzillaReporter[0]['userid'], $resolution, $short_desc, $version, $repPlatform, $everconfirmed, $bug_file_loc, $op_sys
+        );
+
+        $stmt->execute();
+
+        return UbirimiContainer::get()['bugzilla.connection']->insert_id;
+    }
+}
+
+function insertBugzillaComments($bugId, $comments, $short_desc)
+{
+
+    $allComments = '';
+
+    if (empty($comments)) {
+        return;
+    }
+
+    foreach ($comments as $comment) {
+        $bugzillaUser = getBugzillaUserByEmail($comment['email']);
+        $workTime = '0.0';
+        $isPrivate = 0;
+        $already_wrapped = 0;
+        $type = 0;
+        $extra_data = null;
+
+        $query = "INSERT INTO longdescs(
+                bug_id, who, bug_when, work_time, thetext, isprivate, already_wrapped, type, extra_data)
+              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        $stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query);
+
+        $stmt->bind_param(
+            "iisssiiis",
+            $bugId, $bugzillaUser[0]['userid'], $comment['date_created'], $workTime, $comment['content'], $isPrivate, $already_wrapped, $type, $extra_data
+        );
+
+        $stmt->execute();
+
+        $allComments .= $comment['content'];
+    }
+
+    $query = "INSERT INTO bugs_fulltext(
+                bug_id, short_desc, comments, comments_noprivate)
+              VALUES(?, ?, ?, ?)";
+
+    $stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query);
+
+    $stmt->bind_param(
+        "isss",
+        $bugId, $short_desc, $allComments, $allComments
+    );
+
+    $stmt->execute();
+}
+
+function getBugzillaUserByEmail($email)
+{
+    $query = "SELECT *
+            FROM profiles
+            where login_name = ?";
+
+
+    $stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+function getBuzillaProduct($project)
+{
+    $query = "SELECT *
+            FROM products
+            WHERE name = ?
+              AND description = ?";
+
+
+    $stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query);
+    $stmt->bind_param("ss", $project['name'], $project['description']);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+function getBugzillaComponent($component)
+{
+    $query = "SELECT *
+            FROM components
+            WHERE name = ?
+              AND description = ?";
+
+
+    $stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query);
+    $stmt->bind_param("ss", $component['name'], $component['description']);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    return $result->fetch_all(MYSQLI_ASSOC);
 }
 
 function getBugzillaBugBasedOnUbirimiIssue($shortDesc, $creationTs)
@@ -94,51 +317,4 @@ function getBugzillaBugBasedOnUbirimiIssue($shortDesc, $creationTs)
     }
 
     throw new \Exception('getBugzillaBugBasedOnUbirimiIssue Exception');
-}
-
-function insertBugzillaBug($assigned_to, $bug_severity, $bug_status, $component_id, $creation_ts, $date_updated, $priority, $product_id, $reporter, $resolution, $short_desc, $version)
-{
-    $ubirimiUser = User::getById($assigned_to);
-
-    $bugzillaAssignedTo = getBugzillaUserByEmail($assigned_to);
-
-    $query = "INSERT INTO bugs(
-                alias, assigned_to, bug_file_loc, bug_severity, bug_status, cclist_accessible, component_id,
-                creation_ts, deadline, delta_ts, estimated_time, everconfirmed, keywords, lastdiffed, op_sys,
-                priority, product_id, qa_contact, remaining_time, rep_platform, reporter, reporter_accessible,
-                resolution, short_desc, status_whiteboard, target_milestone, version, votes)
-              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-
-    if ($stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query)) {
-        $stmt->bind_param(
-            "sissiissssissssiissiisssssi",
-            null, $bugzillaAssignedTo[0]['user_id'], null, $bug_severity, $bug_status, 1, $component_id,
-            $creation_ts, null, $date_updated, null, 0, null, null, '', $priority, $product_id, null, '0.00', 'All',
-            $reporter, 1, $resolution, $short_desc, '', '---', $version, 0);
-
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            return $result->fetch_array(MYSQLI_ASSOC);
-        }
-
-        return null;
-    }
-}
-
-function getBugzillaUserByEmail($email)
-{
-    $query = "SELECT *
-            FROM profiles
-            where email = " . $email;
-
-
-    $stmt = UbirimiContainer::get()['bugzilla.connection']->prepare($query);
-    $stmt->execute();
-
-    $result = $stmt->get_result();
-
-    return $result->fetch_all(MYSQLI_ASSOC);
 }
