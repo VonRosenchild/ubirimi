5281aaa6ebf7b929186c7cfc5144cbf5606545f4
domnulnopcea <domnulnopcea@gmail.com>
2014-08-24 11:10:36 +0300

    clean activity stream for project

--- src/Ubirimi/Yongo/Resources/views/project/ViewActivityStream.php
+++ src/Ubirimi/Yongo/Resources/views/project/ViewActivityStream.php
@@ -15,61 +15,43 @@
                     <td colspan="3"><b><?php echo Util::getFormattedDate($row['date_created']) ?></b></td>
                 </tr>
             <?php endif ?>
-            <?php if ($date != substr($row['date_created'], 0, 10) || $issueId != $row['issue_id']): ?>
-                <?php if ($date): ?>
-                    <tr>
-                        <td colspan="3" class="sectionDetail">
-
-                        </td>
-                    </tr>
-                <?php endif ?>
-
-                <tr>
-                    <td width="50px" valign="top">
-                        <div style="height: 4px"></div>
-                        <img src="<?php echo User::getUserAvatarPicture(array('avatar_picture' => $row['avatar_picture'],'id' => $row['user_id']), 'big') ?>" />
-                    </td>
-                    <td colspan="2" valing="top" style="vertical-align: top">
-                        <?php if ($row['event'] == 'event_commented'): ?>
-                            <div>
-                                <?php echo LinkHelper::getUserProfileLink($row['user_id'], SystemProduct::SYS_PRODUCT_YONGO, $row['first_name'], $row['last_name']) ?> commented on <a href="/yongo/issue/<?php echo $row['issue_id'] ?>"><?php echo $row['code'] . '-' . $row['nr'] ?></a>
-                            </div>
-                        <?php elseif ($row['event'] == 'event_history'): ?>
-
-                            <?php echo LinkHelper::getUserProfileLink($row['user_id'], SystemProduct::SYS_PRODUCT_YONGO, $row['first_name'], $row['last_name']) ?>
-                            updated <a href="/yongo/issue/<?php echo $row['issue_id'] ?>"><?php echo $row['code'] . '-' . $row['nr'] ?></a>
-
-                        <?php elseif ($row['event'] == 'event_created'): ?>
-                            <div>
-
-                                <?php echo LinkHelper::getUserProfileLink($row['user_id'], SystemProduct::SYS_PRODUCT_YONGO, $row['first_name'], $row['last_name']) ?>
-                                created <a href="/yongo/issue/<?php echo $row['issue_id'] ?>"><?php echo $row['code'] . '-' . $row['nr'] ?></a>
-                                at <?php echo date('H:i', strtotime($row['date_created'])) ?>
-                            </div>
-                        <?php endif ?>
-                    </td>
-                </tr>
-            <?php endif ?>
             <tr>
-                <td></td>
-                <?php if ($row['event'] == 'event_commented'): ?>
-                    <td valign="top" colspan="2" style="word-wrap: break-word;">
+                <td width="50px" valign="top" align="left">
+                    <div style="height: 4px"></div>
+                    <img src="<?php echo User::getUserAvatarPicture(array('avatar_picture' => $row['avatar_picture'],'id' => $row['user_id']), 'big') ?>" />
+                </td>
+                <td colspan="2" valing="top" style="vertical-align: top">
+                    <?php if ($row['event'] == 'event_commented'): ?>
+                        <div>
+                            <?php echo LinkHelper::getUserProfileLink($row['user_id'], SystemProduct::SYS_PRODUCT_YONGO, $row['first_name'], $row['last_name']) ?> commented on <a href="/yongo/issue/<?php echo $row['issue_id'] ?>"><?php echo $row['code'] . '-' . $row['nr'] ?></a>
+                        </div>
                         <?php echo LinkHelper::getUserProfileLink($row['user_id'], SystemProduct::SYS_PRODUCT_YONGO, $row['first_name'], $row['last_name']) ?> commented:
                         <?php echo $row['comment_content'] ?>
-                    </td>
-                <?php elseif ($row['event'] == 'event_history'): ?>
-                    <td colspan="3" valign="top">
+
+                    <?php elseif ($row['event'] == 'event_history'): ?>
+
+                        <?php echo LinkHelper::getUserProfileLink($row['user_id'], SystemProduct::SYS_PRODUCT_YONGO, $row['first_name'], $row['last_name']) ?>
+                        updated <a href="/yongo/issue/<?php echo $row['issue_id'] ?>"><?php echo $row['code'] . '-' . $row['nr'] ?></a>
+
                         <div>
                             updated the <?php echo Field::$fieldTranslation[$row['field']] ?>
                             to <?php echo $row['new_value'] ?>
                         </div>
-                    </td>
-                <?php endif ?>
+                        <div><?php echo Util::getFormattedDate($row['date_created']) ?> &nbsp; <a href="#">Comment</a></div>
+                        <div style="width: 100%; height: 1px; display: block" class="sectionDetail"></div>
+
+                    <?php elseif ($row['event'] == 'event_created'): ?>
+                        <div>
+
+                            <?php echo LinkHelper::getUserProfileLink($row['user_id'], SystemProduct::SYS_PRODUCT_YONGO, $row['first_name'], $row['last_name']) ?>
+                            created <a href="/yongo/issue/<?php echo $row['issue_id'] ?>"><?php echo $row['code'] . '-' . $row['nr'] ?></a>
+                            <div><?php echo Util::getFormattedDate($row['date_created']) ?> &nbsp; <a href="#">Comment</a></div>
+                            <div style="width: 100%; height: 1px; display: block" class="sectionDetail"></div>
+                        </div>
+                    <?php endif ?>
+                </td>
             </tr>
-            <?php
-                $date = substr($row['date_created'], 0, 10);
-                $issueId = $row['issue_id'];
-            ?>
+            <?php $date = substr($row['date_created'], 0, 10); ?>
         <?php endwhile ?>
     </table>
 <?php else: ?>
