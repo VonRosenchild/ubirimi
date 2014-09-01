3e703a1f3cf11bc4506ae947860a78485b29127b
Nopcea Francisc <domnuprofesor@gmail.com>
2014-09-01 10:38:51 +0300

    improved payment ddue notifier message

--- src/Ubirimi/General/Resources/views/_paymentDueMessage.php
+++ src/Ubirimi/General/Resources/views/_paymentDueMessage.php
@@ -1,3 +1,4 @@
-We would like to remind you that there is an outstanding invoice on your account which was due to be paid on 10 August 2014.
+We would like to remind you that there is an outstanding invoice on your account for the month of August.
 
 We fully appreciate that our customers are very busy and that you might have forgotten to pay your invoice. We should be grateful if you can settle the outstanding invoice at your earliest convenience.
+To make the payment you can login here https://my.ubirimi.com/account/pay using your client credentials. You are still enjoying our 50% discount period for all our plans which will end on 31 October 2014.
