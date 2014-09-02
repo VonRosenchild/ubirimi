69e945a9b912a81dbb245afba320e4dec38171d1
Flavius Nopcea <domnulnopcea@gmail.com>
2014-09-02 14:47:53 +0300

    fix delete calendar

---
+++ src/Ubirimi/Calendar/Resources/views/DeleteConfirm.php
@@ -0,0 +1,4 @@
+<div>Are you sure you want to delete this calendar?</div>
+<?php if ($defaultCalendar): ?>
+    <div>This is your primary calendar. This operation can not be undone.</div>
+<?php endif ?>
