95397bc10c09cfc88e2f4b4af0f5f2a5524aeb77
Flavius Nopcea <domnulnopcea@gmail.com>
2014-09-23 16:52:56 +0300

    add function array_equal

--- src/Ubirimi/Util.php
+++ src/Ubirimi/Util.php
@@ -1191,4 +1191,8 @@ class Util {
 
         return $ip;
     }
+
+    public static function array_equal($a, $b) {
+        return (is_array($a) && is_array($b) && array_diff($a, $b) === array_diff($b, $a));
+    }
 }
