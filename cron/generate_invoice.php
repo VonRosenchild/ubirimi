b0bc9f41ce9fb69180decacec810a34bea40bb7c
domnulnopcea <domnulnopcea@gmail.com>
2014-09-23 21:51:09 +0300

    generate invoice only if client is payable and has a paymill id

--- cron/generate_invoice.php
+++ cron/generate_invoice.php
@@ -20,31 +20,33 @@ $invoiceUtil = new \Ubirimi\InvoiceUtil();
 $paymentUtil = new PaymentUtil();
 
 while ($client = $clients->fetch_array(MYSQLI_ASSOC)) {
-    $dayClientCreated = substr($client['date_created'], 8, 2);
+    if ($client['is_payable'] && $client['paymill_id']) {
+        $dayClientCreated = substr($client['date_created'], 8, 2);
 
-    // do not generate an invoice in the first month of usage
-    if (substr($client['date_created'], 0, 7) != date('Y-m')) {
-        // get last invoice for the current month
-        $lastInvoice = $invoiceUtil->getLastByClientIdAndMonthAndYear($client['id'], date('m'), date('Y'));
-        if (!$lastInvoice) {
-            $lastInvoice = $invoiceUtil->getLast();
-            $lastNumber = $lastInvoice['number'];
-            $lastNumber++;
+        // do not generate an invoice in the first month of usage
+        if (substr($client['date_created'], 0, 7) != date('Y-m')) {
+            // get last invoice for the current month
+            $lastInvoice = $invoiceUtil->getLastByClientIdAndMonthAndYear($client['id'], date('m'), date('Y'));
+            if (!$lastInvoice) {
+                $lastInvoice = $invoiceUtil->getLast();
+                $lastNumber = $lastInvoice['number'];
+                $lastNumber++;
 
-            if (checkdate(date('n'), $dayClientCreated, date('Y'))) {
-                $invoiceDate = date('Y-m') . '-' . $dayClientCreated;
-            } else {
-                $dayClientCreated--;
-                $invoiceDate = date('Y-m') . '-' . $dayClientCreated;
+                if (checkdate(date('n'), $dayClientCreated, date('Y'))) {
+                    $invoiceDate = date('Y-m') . '-' . $dayClientCreated;
+                } else {
+                    $dayClientCreated--;
+                    $invoiceDate = date('Y-m') . '-' . $dayClientCreated;
+                }
+                $amount = $paymentUtil->getAmountByClientId($client['id']);
+                $VAT = 0;
+                if (in_array($client['sys_country_id'], array_keys(PaymentUtil::$VATValuePerCountry))) {
+                    $VAT = $amount * PaymentUtil::$VATValuePerCountry[$client['sys_country_id']] / 100;
+                }
+                $totalToBeCharged = $amount + $VAT;
+
+                $invoiceUtil->generate($client['id'], $totalToBeCharged, $lastNumber, $invoiceDate);
             }
-            $amount = $paymentUtil->getAmountByClientId($client['id']);
-            $VAT = 0;
-            if (in_array($client['sys_country_id'], array_keys(PaymentUtil::$VATValuePerCountry))) {
-                $VAT = $amount * PaymentUtil::$VATValuePerCountry[$client['sys_country_id']] / 100;
-            }
-            $totalToBeCharged = $amount + $VAT;
-
-            $invoiceUtil->generate($client['id'], $totalToBeCharged, $lastNumber, $invoiceDate);
         }
     }
 }
