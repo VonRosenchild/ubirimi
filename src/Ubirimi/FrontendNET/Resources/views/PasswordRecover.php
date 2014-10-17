ed4f48b571e550cdd26eabc6aec9cff9fec724fd
Flavius Nopcea <domnulnopcea@gmail.com>
2014-10-17 15:06:22 +0300

    center recover password page

--- src/Ubirimi/FrontendNET/Resources/views/PasswordRecover.php
+++ src/Ubirimi/FrontendNET/Resources/views/PasswordRecover.php
@@ -1,17 +1,23 @@
 <div class="container page-container recover-user-password">
-    <h1>Recover my password</h1>
+    <table align="center">
+        <tr>
+            <td>
+                <h1>Recover my password</h1>
 
-    <div class="clearfix">
+                <div class="clearfix">
 
-        <p>
-            Simply enter the email address associated with your account below <br />
-            and a new password will be sent to your email address immediately.
-        </p>
-        <div class="align-left">
-            <?php require_once __DIR__ . '/_passwordRecoverForm.php' ?>
+                    <p>
+                        Simply enter the email address associated with your account below <br />
+                        and a new password will be sent to your email address immediately.
+                    </p>
+                    <div class="align-left">
+                        <?php require_once __DIR__ . '/_passwordRecoverForm.php' ?>
 
-            <button class="button_hp blue" type="button" name="retrieve">Restore Password</button>
-            <img id="loader" src="/img/loader.gif" style="display: none;"/>
-        </div>
-    </div>
+                        <button class="button_hp blue" type="button" name="retrieve">Restore Password</button>
+                        <img id="loader" src="/img/loader.gif" style="display: none;"/>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
 </div>
