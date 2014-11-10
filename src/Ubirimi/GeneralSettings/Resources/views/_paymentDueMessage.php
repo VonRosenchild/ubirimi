9966036de39a5cdfd8ea9267ee0019158d1fe8d9
Flavius Nopcea <flavius_nopcea@netex.ro>
2014-11-10 13:06:59 +0200

    remove unneeded data

--- src/Ubirimi/GeneralSettings/Resources/views/_paymentDueMessage.php
+++
@@ -1,4 +0,0 @@
-We would like to remind you that there is an outstanding invoice on your account for the month of August.
-
-We fully appreciate that our customers are very busy and that you might have forgotten to pay your invoice. We should be grateful if you can settle the outstanding invoice at your earliest convenience.
-To make the payment you can login here https://my.ubirimi.com/account/pay using your client credentials. You are still enjoying our 50% discount period for all our plans which will end on 31 October 2014.
