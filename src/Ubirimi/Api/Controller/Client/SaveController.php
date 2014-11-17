bd342dacf33f4dd9a3cdb8149d3f1433393417b9
Francisc Nopcea <domnuprofesor@gmail.com>
2014-11-17 17:48:21 +0200

    save client api

---
+++ src/Ubirimi/Api/Controller/Client/SaveController.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ *  Copyright (C) 2012-2014 SC Ubirimi SRL <info-copyright@ubirimi.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Ubirimi\Api\Controller\Client;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Ubirimi\Container\UbirimiContainer;
+use Ubirimi\Repository\GeneralTaskQueue;
+use Ubirimi\UbirimiController;
+
+class SaveController extends UbirimiController
+{
+    public function indexAction(Request $request, SessionInterface $session)
+    {
+        UbirimiContainer::get()['api.auth']->auth($request);
+
+        $this->getRepository(GeneralTaskQueue::class)->savePendingClientData($request->getContent());
+    }
+}
