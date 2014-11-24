8af18020727482a431fc739c4b600b9cbbd963ad
Francisc Nopcea <domnuprofesor@gmail.com>
2014-11-24 10:08:01 +0200

    close https://github.com/ubirimi/ubirimi/issues/3

--- src/Ubirimi/Service/PasswordService.php
+++ src/Ubirimi/Service/PasswordService.php
@@ -19,27 +19,15 @@
 
 namespace Ubirimi\Service;
 
-use Ubirimi\PasswordHash;
-
 class PasswordService
 {
-    /**
-     * @var \Ubirimi\PasswordHash;
-     */
-    private $hasher;
-
-    public function __construct()
-    {
-        $this->hasher = new PasswordHash(8, false);
-    }
-
     public function check($plainPassword, $hashedPassword)
     {
-        return $this->hasher->CheckPassword($plainPassword, $hashedPassword);
+        return password_verify($plainPassword, $hashedPassword);
     }
 
     public function hash($plainPassword)
     {
-        return $this->hasher->HashPassword($plainPassword);
+        return password_hash($plainPassword, PASSWORD_DEFAULT);
     }
 }
