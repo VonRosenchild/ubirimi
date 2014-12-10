ac993e474f7ba090e1762a96cfce472e470e60ac
Francisc Nopcea <domnuprofesor@gmail.com>
2014-12-10 12:57:04 +0200

    fixes for filter subscription

--- cron/issue/run_filter_subscription.php
+++ cron/issue/run_filter_subscription.php
@@ -31,20 +31,25 @@ foreach ($searchParametersInFilter as $searchParameter) {
     $data = explode('=', $searchParameter);
     $searchParameters[$data[0]] = $data[1];
 }
-$user = UbirimiContainer::get()['respository']->get(UbirimiUser::class)->getById($filter['user_id']);
+$user = UbirimiContainer::get()['repository']->get(UbirimiUser::class)->getById($filter['user_id']);
 $smtpSettings = UbirimiContainer::get()['repository']->get(SMTPServer::class)->getByClientId($user['client_id']);
-$clientSettings = UbirimiContainer::get()['respository']->get(UbirimiClient::class)->getSettings($user['client_id']);
 
-$client = UbirimiContainer::get()['respository']->get(UbirimiClient::class)->getById($user['client_id']);
+if (!$smtpSettings) {
+    die('No SMPT server defined');
+}
+
+$clientSettings = UbirimiContainer::get()['repository']->get(UbirimiClient::class)->getSettings($user['client_id']);
+
+$client = UbirimiContainer::get()['repository']->get(UbirimiClient::class)->getById($user['client_id']);
 $subject = $smtpSettings['email_prefix'] . " Filter - " . $filter['name'];
 
 $usersToNotify = array();
 
 if ($filterSubscription['user_id']) {
-    $user = UbirimiContainer::get()['respository']->get(UbirimiUser::class)->getById($filterSubscription['user_id']);
+    $user = UbirimiContainer::get()['repository']->get(UbirimiUser::class)->getById($filterSubscription['user_id']);
     $usersToNotify[] = $user;
 } else if ($filterSubscription['group_id']) {
-    $users = UbirimiContainer::get()['respository']->get(UbirimiGroup::class)->getDataByGroupId($filterSubscription['group_id']);
+    $users = UbirimiContainer::get()['repository']->get(UbirimiGroup::class)->getDataByGroupId($filterSubscription['group_id']);
     while ($users && $user = $users->fetch_array(MYSQLI_ASSOC)) {
         $usersToNotify[] = $user;
     }
@@ -55,7 +60,7 @@ foreach ($usersToNotify as $user) {
 
     $columns = explode('#', $user['issues_display_columns']);
 
-    UbirimiContainer::get()['repository']->get(EmailQueue::class)->oadd($user['client_id'],
+    UbirimiContainer::get()['repository']->get(EmailQueue::class)->add($user['client_id'],
         $smtpSettings['from_address'],
         $user['email'],
         null,
--- cron/issue/update_filter_subscription.php
+++ cron/issue/update_filter_subscription.php
@@ -13,13 +13,16 @@ if (file_exists(__DIR__ . '/update_filter_subscription.lock')) {
 }
 
 require_once __DIR__ . '/../../web/bootstrap_cli.php';
+
 $filterSubscriptions = UbirimiContainer::get()['repository']->get(IssueFilter::class)->getAllSubscriptions();
-$crontabLines = '*/10 * * * * /usr/bin/php /home/ubirimi-web/cron/issue/update_filter_subscription.php' . "\n";
+$crontabLines = '*/10 * * * * /usr/bin/php ' . getcwd() . '/update_filter_subscription.php' . "\n";
 
 while ($filterSubscriptions && $filterSubscription = $filterSubscriptions->fetch_array(MYSQLI_ASSOC)) {
-    $crontabLines .= $filterSubscription['period'] . ' /usr/bin/php /home/ubirimi_web/cron/issue/run_filter_subscription.php ' . $filterSubscription['id'] . "\n";
+    $crontabLines .= $filterSubscription['period'] . ' /usr/bin/php ' . getcwd() . '/run_filter_subscription.php ' . $filterSubscription['id'] . PHP_EOL;
 }
-system('echo ' . $crontabLines . " | crontab");
+
+system('echo "' . $crontabLines . '" | crontab');
+
 if (null !== $fp) {
     fclose($fp);
 }
