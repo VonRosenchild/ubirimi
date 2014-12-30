e76730fbc783fd178164e6f8675b75729f79d662
Francisc Nopcea <domnuprofesor@gmail.com>
2014-12-30 10:38:12 +0200

    set svn notifications by email to be sent from the customer smtp server

--- src/Ubirimi/SvnHosting/Service/EmailService.php
+++ src/Ubirimi/SvnHosting/Service/EmailService.php
@@ -28,30 +28,34 @@ class EmailService extends UbirimiService
 {
     public function newUser($repositoryName, $firstName, $lastName, $username, $mail, $repositoryName)
     {
-        Email::$smtpSettings = Util::getUbirimiSMTPSettings();
-        UbirimiContainer::get()['repository']->get(Email::class)->sendNewUserRepositoryNotificationEmail($this->session->get('client/id'), $firstName, $lastName, $username, null, $mail, $repositoryName);
+        if ($this->session->get('client/settings/smtp')) {
+
+            Email::$smtpSettings = $this->session->get('client/settings/smtp');
+            UbirimiContainer::get()['repository']->get(Email::class)->sendNewUserRepositoryNotificationEmail($this->session->get('client/id'), $firstName, $lastName, $username, null, $mail, $repositoryName);
+        }
     }
 
     public function passwordUpdate($repositoryName, $user, $password)
     {
-        Email::$smtpSettings = Util::getUbirimiSMTPSettings();
-        UbirimiContainer::get()['repository']->get(Email::class)->sendUserChangedPasswordForRepositoryNotificationEmail(
-            $this->session->get('client/id'),
-            $user['first_name'],
-            $user['last_name'],
-            $user['username'],
-            $password,
-            $user['email'],
-            $repositoryName
-        );
+        if ($this->session->get('client/settings/smtp')) {
+            Email::$smtpSettings = $this->session->get('client/settings/smtp');
+
+            UbirimiContainer::get()['repository']->get(Email::class)->sendUserChangedPasswordForRepositoryNotificationEmail(
+                $this->session->get('client/id'),
+                $user['first_name'],
+                $user['last_name'],
+                $user['username'],
+                $password,
+                $user['email'],
+                $repositoryName
+            );
+        }
     }
 
     public function importUsers($repositoryName, $user)
     {
-        $smtpSettings = $this->session->get('client/settings/smtp');
-
-        if ($smtpSettings) {
-            Email::$smtpSettings = $smtpSettings;
+        if ($this->session->get('client/settings/smtp')) {
+            Email::$smtpSettings = $this->session->get('client/settings/smtp');
             UbirimiContainer::get()['repository']->get(Email::class)->sendNewUserRepositoryNotificationEmail(
                 $this->session->get('client/id'),
                 $user['first_name'],
