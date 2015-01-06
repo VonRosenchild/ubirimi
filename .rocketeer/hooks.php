ac20fb8448e22bfc35998b3445d68ceb95e997f5
Francisc Nopcea <domnuprofesor@gmail.com>
2015-01-06 15:24:00 +0200

    fixes

---
+++ .rocketeer/hooks.php
@@ -0,0 +1,39 @@
+<?php return array(
+
+	// Tasks
+	//
+	// Here you can define in the `before` and `after` array, Tasks to execute
+	// before or after the core Rocketeer Tasks. You can either put a simple command,
+	// a closure which receives a $task object, or the name of a class extending
+	// the Rocketeer\Abstracts\AbstractTask class
+	//
+	// In the `custom` array you can list custom Tasks classes to be added
+	// to Rocketeer. Those will then be available in the command line
+	// with all the other tasks
+	//////////////////////////////////////////////////////////////////////
+
+	// Tasks to execute before the core Rocketeer Tasks
+	'before' => array(
+		'setup'   => array(),
+		'deploy'  => array(),
+		'cleanup' => array(),
+	),
+
+	// Tasks to execute after the core Rocketeer Tasks
+	'after'  => array(
+		'setup'   => array(),
+		'deploy'  => array(
+			'git clone git@bitbucket.org:ubirimi137/configurations.git',
+			'mv configurations/products/config.properties app/config/config.properties',
+			'rm -rf configurations',
+			'rm -rf /home/products/cache',
+			'mkdir -R /home/products/cache',
+			'chown www-data:www-data -R /home/products/cache'
+		),
+		'cleanup' => array(),
+	),
+
+	// Custom Tasks to register with Rocketeer
+	'custom' => array(),
+
+);
