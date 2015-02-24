b4ee33e0c2dcfecd5a79f2ba90801a80afc314e6
domnulnopcea <domnulnopcea@gmail.com>
2015-02-24 14:30:05 +0200

    introduce cron to reload apache configuration

---
+++ cron/reload_apache.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ *  Copyright (C) 2012-2014 SC Ubirimi SRL <info-copyright@ubirimi.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+use Ubirimi\Container\UbirimiContainer;
+use Ubirimi\SvnHosting\Repository\SvnRepository;
+
+require_once __DIR__ . '/../web/bootstrap_cli.php';
+
+$repositoriesLastMinute = UbirimiContainer::get()['repository']->get(SvnRepository::class)->getBatch();
+
+if (count($repositoriesLastMinute)) {
+    system("/etc/init.d/apache2 reload");
+}
